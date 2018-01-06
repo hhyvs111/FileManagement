7fb3db69a51c3a869ee96f2baf68d20c9ee260b8
SmallNancy <smallnancy@outlook.com>
2018-01-06 20:46:02 +0800

    qss

--- FileManagement/main.cpp
+++ FileManagement/main.cpp
@@ -3,10 +3,26 @@
 #include "Register.h"
 #include "Database.h"
 #include <QtWidgets/QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+
+	QString qss;
+	QFile qssFile("mystyle.qss");
+	qssFile.open(QFile::ReadOnly);
+	if (qssFile.isOpen())
+	{
+		qss = QLatin1String(qssFile.readAll());
+		a.setStyleSheet(qss);
+		qssFile.close();
+	}
+	else
+	{
+		qDebug("Open failed");
+	}
+
 	FileManagement F;
 	Login L; 
 	Register R;
